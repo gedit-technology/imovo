70f369c70a88ca4b5c7d4759ef42271b0ab52562
Sébastien Wilmet <sw@swilmet.be>
2021-05-18 01:18:57 +0200

    gtksourceview: imovo/gedit stuff at the top of the script

--- src/gtksourceview.sh
+++ src/gtksourceview.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+tarballs_cache_dir=~/.cache/imovo/tarballs
+build_prefix='/tmp/imovo'
+install_prefix=~/.imovo/gedit
+rm -rf "${install_prefix}"
+
 tarball_filename='gtksourceview-4.0.40.tar.xz'
 remote_tarball_url="https://github.com/gedit-org/gtksourceview/releases/download/4.0.40/${tarball_filename}"
-tarballs_cache_dir=~/.cache/imovo/tarballs
 tarball_path="${tarballs_cache_dir}/${tarball_filename}"
 
 if test ! -f "${tarball_path}"
@@ -11,17 +15,14 @@ then
 	curl --output "${tarball_path}" --location "${remote_tarball_url}" || exit 1
 fi
 
-build_prefix='/tmp/imovo'
 rm -rf "${build_prefix}"
 mkdir -p "${build_prefix}"
-
 pushd "${build_prefix}"
+
 cp "${tarball_path}" .
 tar xf "${tarball_filename}"
 pushd "gtksourceview-4.0.40/"
 
-install_prefix=~/.imovo/gedit
-rm -rf "${install_prefix}"
 mkdir -p "${install_prefix}"
 ./configure --prefix="${install_prefix}" --enable-introspection=yes || exit 1
 make || exit 1
