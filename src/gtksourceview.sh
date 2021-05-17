b1edfff030f907d7545f1d7bc40144cb604afcdb
Sébastien Wilmet <sw@swilmet.be>
2021-05-17 23:53:17 +0200

    gtksourceview: continue

--- src/gtksourceview.sh
+++ src/gtksourceview.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-remote_tarball_url='https://github.com/gedit-org/gtksourceview/releases/download/4.0.40/gtksourceview-4.0.40.tar.xz'
+tarball_filename='gtksourceview-4.0.40.tar.xz'
+remote_tarball_url="https://github.com/gedit-org/gtksourceview/releases/download/4.0.40/${tarball_filename}"
 tarballs_cache_dir=~/.cache/imovo/tarballs
-tarball_path="${tarballs_cache_dir}/gtksourceview-4.0.40.tar.xz"
+tarball_path="${tarballs_cache_dir}/${tarball_filename}"
 
 if test ! -f "${tarball_path}"
 then
 	mkdir -p "${tarballs_cache_dir}"
-	curl --output "${tarball_path}" --location "${remote_tarball_url}"
+	curl --output "${tarball_path}" --location "${remote_tarball_url}" || exit 1
 fi
+
+build_prefix='/tmp/imovo'
+rm -rf "${build_prefix}"
+mkdir -p "${build_prefix}"
+
+pushd "${build_prefix}"
+cp "${tarball_path}" .
+tar xf "${tarball_filename}"
