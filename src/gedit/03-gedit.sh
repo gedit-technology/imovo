d76a47e507c42456e45aed5031b18c21e9931e63
Sébastien Wilmet <sw@swilmet.be>
2021-05-19 06:50:16 +0200

    gedit: continue

--- src/gedit/03-gedit.sh
+++ src/gedit/03-gedit.sh
@@ -5,6 +5,7 @@ install_prefix=~/.imovo/gedit
 
 git_repo_remote_url='https://gitlab.gnome.org/GNOME/gedit.git'
 git_repo_path="${git_repos_cache_dir}/gedit"
+git_repo_commit='4e971d68e320094258f63b6a907de4b48b46f33d'
 
 if test ! -d "${git_repo_path}"
 then
@@ -14,19 +15,20 @@ then
 	popd
 fi
 
-#pushd "${git_repo_path}"
-#git clean -xdf
-#git r
-#git checkout master
-#
-#mkdir build
-#pushd build/
-#
-#export PKG_CONFIG_PATH="${install_prefix}/lib64/pkgconfig"
-#
-## At least for g-ir-scanner.
-#export XDG_DATA_DIRS="${install_prefix}/share:${XDG_DATA_DIRS}"
-#
+pushd "${git_repo_path}"
+git clean -xdf
+git r
+git branch -D imovo
+git checkout -b imovo "${git_repo_commit}"
+
+mkdir build
+pushd build/
+
+export PKG_CONFIG_PATH="${install_prefix}/lib64/pkgconfig"
+
+# At least for g-ir-scanner.
+export XDG_DATA_DIRS="${install_prefix}/share:${XDG_DATA_DIRS}"
+
 #meson --prefix "${install_prefix}" || exit 1
 #ninja || exit 1
 #ninja test || exit 1 # before install
