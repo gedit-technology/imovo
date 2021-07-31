1a9cb19c25badf4c0d5ad32c042987bef29c3026
Sébastien Wilmet <sw@swilmet.be>
2021-07-31 19:57:41 +0200

    gnome-latex: start 04-gnome-latex.sh impl

---
+++ src/gnome-latex/04-gnome-latex.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+git_repos_cache_dir=~/.cache/imovo/git-repos
+install_prefix=~/.imovo/gnome-latex
+
+git_repo_remote_url='https://gitlab.gnome.org/Archive/gnome-latex.git'
+git_repo_path="${git_repos_cache_dir}/gnome-latex"
+
+if test ! -d "${git_repo_path}"
+then
+	mkdir -p "${git_repos_cache_dir}"
+	pushd "${git_repos_cache_dir}"
+	git clone "${git_repo_remote_url}" || exit 1
+	popd
+fi
+
+pushd "${git_repo_path}"
+git clean -xdf
+git r
+git checkout master
+
+popd
