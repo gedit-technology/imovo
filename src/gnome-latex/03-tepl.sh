d6b51ca38818a9731fc7055e108d60b23abd5c6b
Sébastien Wilmet <sw@swilmet.be>
2021-07-31 18:45:23 +0200

    gnome-latex: LD_LIBRARY_PATH

--- src/gnome-latex/03-tepl.sh
+++ src/gnome-latex/03-tepl.sh
@@ -27,6 +27,8 @@ export PKG_CONFIG_PATH="${install_prefix}/lib64/pkgconfig"
 # At least for g-ir-scanner.
 export XDG_DATA_DIRS="${install_prefix}/share:${XDG_DATA_DIRS}"
 
+export LD_LIBRARY_PATH="${install_prefix}/lib64"
+
 meson --prefix "${install_prefix}" || exit 1
 ninja || exit 1
 ninja test || exit 1 # before install
