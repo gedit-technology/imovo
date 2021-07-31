3535a99adb7eb3db08bfdf50f99a1c181fad9b36
Sébastien Wilmet <sw@swilmet.be>
2021-07-31 21:01:29 +0200

    gnome-latex: 04-gnome-latex.sh: XDG_DATA_DIRS

--- src/gnome-latex/04-gnome-latex.sh
+++ src/gnome-latex/04-gnome-latex.sh
@@ -21,6 +21,9 @@ git checkout master
 
 export PKG_CONFIG_PATH="${install_prefix}/lib64/pkgconfig"
 
+# At least for g-ir-scanner.
+export XDG_DATA_DIRS="${install_prefix}/share:${XDG_DATA_DIRS}"
+
 ./autogen.sh --prefix="${install_prefix}" --enable-introspection --enable-vala --disable-dconf-migration || exit 1
 make || exit 1
 
