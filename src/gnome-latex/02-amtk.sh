aed484c40fe033163ef606cbec0b0f16db1e02f9
Sébastien Wilmet <sw@swilmet.be>
2021-07-31 17:41:54 +0200

    gnome-latex: simplify 02-amtk.sh

--- src/gnome-latex/02-amtk.sh
+++ src/gnome-latex/02-amtk.sh
@@ -22,11 +22,6 @@ git checkout master
 mkdir build
 pushd build/
 
-export PKG_CONFIG_PATH="${install_prefix}/lib64/pkgconfig"
-
-# At least for g-ir-scanner.
-export XDG_DATA_DIRS="${install_prefix}/share:${XDG_DATA_DIRS}"
-
 meson --prefix "${install_prefix}" || exit 1
 ninja || exit 1
 ninja test || exit 1 # before install
@@ -35,7 +30,7 @@ ninja test || exit 1 # after install
 
 popd
 popd
-if pkg-config --modversion amtk-5
+if PKG_CONFIG_PATH="${install_prefix}/lib64/pkgconfig" pkg-config --modversion amtk-5
 then
 	echo OK
 else
