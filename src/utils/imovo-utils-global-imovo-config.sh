680458f085a503b6d72d737d67d96dd4227e4d27
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2021-09-10 22:22:30 +0200

    utils: global-imovo-config: add a var

--- src/utils/imovo-utils-global-imovo-config.sh
+++ src/utils/imovo-utils-global-imovo-config.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 imovo_config_git_repos_location=~/comp/gedit-org
-imovo_config_tarballs_location=~/.imovo/_tarballs
-imovo_config_tmp_dir=~/.imovo/_tmp
+
+imovo_config_prefix=~/.imovo
+imovo_config_tarballs_location="${imovo_config_prefix}/_tarballs"
+imovo_config_tmp_dir="${imovo_config_prefix}/_tmp"
