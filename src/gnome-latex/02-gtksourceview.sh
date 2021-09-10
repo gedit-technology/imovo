ea677fdc02c1c946f6daac722ceb45c742e97e99
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2021-09-10 15:07:53 +0200

    gnome-latex: 02-gtksourceview.sh: start

---
+++ src/gnome-latex/02-gtksourceview.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+tarball_url='https://github.com/gedit-org/gtksourceview/releases/download/4.0.40/gtksourceview-4.0.40.tar.xz'
+tarball_filename='gtksourceview-4.0.40.tar.xz'
+
+../utils/imovo-utils-cache-tarball.sh "${tarball_filename}" "${tarball_url}"
