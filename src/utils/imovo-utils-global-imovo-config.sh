bc41f741efd30d8a0bcacb6b0a8d1e1682e57c4b
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2021-09-10 21:53:41 +0200

    utils: add a global imovo_config var

--- src/utils/imovo-utils-global-imovo-config.sh
+++ src/utils/imovo-utils-global-imovo-config.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 imovo_config_tarballs_location=~/.imovo/_tarballs
+imovo_config_tmp_dir=~/.imovo/_tmp
--- src/utils/imovo-utils-prepare-module-source-tarball.sh
+++ src/utils/imovo-utils-prepare-module-source-tarball.sh
@@ -3,13 +3,12 @@
 tarball_filename="$1"
 
 source ./imovo-utils-global-imovo-config.sh
-imovo_tmp_prefix=~/.imovo/_tmp
 
-rm -rf "${imovo_tmp_prefix}"
-mkdir -p "${imovo_tmp_prefix}"
+rm -rf "${imovo_config_tmp_dir}"
+mkdir -p "${imovo_config_tmp_dir}"
 
-cp "${imovo_config_tarballs_location}/${tarball_filename}" "${imovo_tmp_prefix}/"
+cp "${imovo_config_tarballs_location}/${tarball_filename}" "${imovo_config_tmp_dir}/"
 
-pushd "${imovo_tmp_prefix}/"
+pushd "${imovo_config_tmp_dir}/"
 tar xf "${tarball_filename}"
 popd
