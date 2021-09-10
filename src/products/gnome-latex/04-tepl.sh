7a9ff3a7c9b0cfab792f9b8a81851a343ae42f06
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2021-09-11 00:52:13 +0200

    products/gnome-latex: tepl: start

---
+++ src/products/gnome-latex/04-tepl.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+pushd ../../modules/
+./tepl.sh 'gnome-latex'
+popd
--- src/products/gnome-latex/all.sh
+++ src/products/gnome-latex/all.sh
@@ -3,3 +3,4 @@
 ./01-prepa.sh || exit 1
 ./02-gtksourceview.sh || exit 1
 ./03-amtk.sh || exit 1
+./04-tepl.sh || exit 1
