2bdc8c4db1cdddc9488442f1d0693b9a88a14548
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2021-09-10 23:17:24 +0200

    gnome-latex: gtksourceview: delegate to modules/

--- src/gnome-latex/02-gtksourceview.sh
+++ src/gnome-latex/02-gtksourceview.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-tarball_url='https://github.com/gedit-org/gtksourceview/releases/download/4.0.40/gtksourceview-4.0.40.tar.xz'
-tarball_filename='gtksourceview-4.0.40.tar.xz'
-
-pushd ../utils/
-./imovo-utils-cache-tarball.sh "${tarball_filename}" "${tarball_url}"
-./imovo-utils-prepare-module-source-tarball.sh "${tarball_filename}"
+pushd ../modules/
+./gtksourceview.sh 'gnome-latex'
 popd
