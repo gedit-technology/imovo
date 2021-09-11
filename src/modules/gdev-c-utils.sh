f82a3c9d947743daa9ee0c33e3a0c0063d6cecd6
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2021-09-11 18:51:11 +0200

    modules: add gdev-c-utils

---
+++ src/modules/gdev-c-utils.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+product="$1"
+source ../utils/imovo-utils-global-imovo-config.sh
+install_prefix="${imovo_config_prefix}/${product}"
+
+pushd ../utils/
+./imovo-utils-prepare-module-source-git.sh 'gdev-c-utils' 'master'
+popd
+
+pushd "${imovo_config_tmp_dir}/gdev-c-utils/build/"
+meson --prefix "${install_prefix}" || exit 1
+ninja || exit 1
+ninja test || exit 1 # before install
+ninja install || exit 1
+ninja test || exit 1 # after install
+popd
