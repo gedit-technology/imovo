00a2a34ec31ff2b3cf763680dbd618908cc2f9d2
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2021-09-11 02:20:29 +0200

    products/gnome-latex: done, v2

--- src/products/gnome-latex/05-gnome-latex.sh
+++ src/products/gnome-latex/05-gnome-latex.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-install_prefix=~/.imovo/gnome-latex
+source ../../utils/imovo-utils-global-imovo-config.sh
+install_prefix="${imovo_config_prefix}/gnome-latex"
 
 export PKG_CONFIG_PATH="${install_prefix}/lib64/pkgconfig"
 
 # At least for g-ir-scanner.
 export XDG_DATA_DIRS="${install_prefix}/share:${XDG_DATA_DIRS}"
+
+pushd ../../modules/
+./gnome-latex.sh 'gnome-latex'
+popd
--- src/products/gnome-latex/all.sh
+++ src/products/gnome-latex/all.sh
@@ -4,3 +4,4 @@
 ./02-gtksourceview.sh || exit 1
 ./03-amtk.sh || exit 1
 ./04-tepl.sh || exit 1
+./05-gnome-latex.sh || exit 1
