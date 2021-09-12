94d30603f9c73943208f4eb83d0c396d91125c49
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2021-09-13 00:02:07 +0200

    utils: prepare-module-source-git: make it more robust

--- src/utils/imovo-utils-prepare-module-source-git.sh
+++ src/utils/imovo-utils-prepare-module-source-git.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
+if test $# -ne 2
+then
+	echo "$0: wrong number of arguments." 1>&2
+	exit 1
+fi
+
 git_repo_name="$1"
 git_ref="$2"
 
 source ./imovo-utils-global-imovo-config.sh
 
-rm -rf "${imovo_config_tmp_dir}"
-mkdir -p "${imovo_config_tmp_dir}"
+rm -rf "${imovo_config_tmp_dir}" || exit 1
+mkdir -p "${imovo_config_tmp_dir}" || exit 1
 
-cp -r "${imovo_config_git_repos_location}/${git_repo_name}" "${imovo_config_tmp_dir}/"
+cp -r "${imovo_config_git_repos_location}/${git_repo_name}" "${imovo_config_tmp_dir}/" || exit 1
 
 pushd "${imovo_config_tmp_dir}/${git_repo_name}/"
-git clean -xdf
-git reset --hard HEAD
-git checkout -b imovo "${git_ref}"
+git clean -xdf || exit 1
+git reset --hard HEAD || exit 1
+git checkout -b imovo "${git_ref}" || exit 1
 popd
