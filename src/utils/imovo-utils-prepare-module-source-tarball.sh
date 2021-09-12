cd449aed35be572998eea282355bf40b291f2f94
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2021-09-12 23:55:35 +0200

    utils: prepare-module-source-tarball: make it more robust

--- src/utils/imovo-utils-prepare-module-source-tarball.sh
+++ src/utils/imovo-utils-prepare-module-source-tarball.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
 
+if test $# -ne 1
+then
+	echo "$0: wrong number of arguments." 1>&2
+	exit 1
+fi
+
 tarball_filename="$1"
 
 source ./imovo-utils-global-imovo-config.sh
 
-rm -rf "${imovo_config_tmp_dir}"
-mkdir -p "${imovo_config_tmp_dir}"
+rm -rf "${imovo_config_tmp_dir}" || exit 1
+mkdir -p "${imovo_config_tmp_dir}" || exit 1
 
-cp "${imovo_config_tarballs_location}/${tarball_filename}" "${imovo_config_tmp_dir}/"
+cp "${imovo_config_tarballs_location}/${tarball_filename}" "${imovo_config_tmp_dir}/" || exit 1
 
 pushd "${imovo_config_tmp_dir}/"
-tar xf "${tarball_filename}"
+tar xf "${tarball_filename}" || exit 1
 popd
