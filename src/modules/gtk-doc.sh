8467254bc0f078e11f77db72f2fbd20e07977635
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2021-09-13 01:31:06 +0200

    modules/gtk-doc: use util
    The difference is that now tests are run.

--- src/modules/gtk-doc.sh
+++ src/modules/gtk-doc.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
+if test $# -ne 1
+then
+	echo "$0: wrong number of arguments." 1>&2
+	exit 1
+fi
+
 product="$1"
-source ../utils/imovo-utils-global-imovo-config.sh
-install_prefix="${imovo_config_prefix}/${product}"
 
 pushd ../utils/
-./imovo-utils-prepare-module-source-git.sh 'gtk-doc' 'master'
-popd
-
-pushd "${imovo_config_tmp_dir}/gtk-doc/"
-mkdir build
-pushd build/
-meson --prefix "${install_prefix}" || exit 1
-ninja || exit 1
-ninja install || exit 1
-popd
+./imovo-utils-build-module-standard-meson.sh "${product}" 'gtk-doc' 'master' || exit 1
 popd
