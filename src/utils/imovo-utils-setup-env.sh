9758e7cbbe9333e622d7c3ad49fd3b1f4ba0f17b
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2021-09-13 20:24:58 +0200

    utils: setup-env: var names prefixed with an underscore

--- src/utils/imovo-utils-setup-env.sh
+++ src/utils/imovo-utils-setup-env.sh
@@ -6,11 +6,11 @@ then
 	exit 1
 fi
 
-product="$1"
+_product="$1"
 
 source ./imovo-utils-global-imovo-config.sh
-install_prefix="${imovo_config_prefix}/${product}"
+_install_prefix="${imovo_config_prefix}/${_product}"
 
-export PKG_CONFIG_PATH="${install_prefix}/lib64/pkgconfig"
-export XDG_DATA_DIRS="${install_prefix}/share:${XDG_DATA_DIRS}"
-export LD_LIBRARY_PATH="${install_prefix}/lib64"
+export PKG_CONFIG_PATH="${_install_prefix}/lib64/pkgconfig"
+export XDG_DATA_DIRS="${_install_prefix}/share:${XDG_DATA_DIRS}"
+export LD_LIBRARY_PATH="${_install_prefix}/lib64"
