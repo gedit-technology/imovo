2b0fbd4b1df0373689d3bf3e6a6e1029109494c7
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2021-09-13 20:56:50 +0200

    products/gdev-c-utils: use util

--- src/products/gdev-c-utils/05-hack-on-gdev-c-utils.sh
+++ src/products/gdev-c-utils/05-hack-on-gdev-c-utils.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-source ../../utils/imovo-utils-global-imovo-config.sh
-install_prefix="${imovo_config_prefix}/gdev-c-utils"
-
-export PKG_CONFIG_PATH="${install_prefix}/lib64/pkgconfig"
-
-# At least for g-ir-scanner.
-export XDG_DATA_DIRS="${install_prefix}/share:${XDG_DATA_DIRS}"
-
-export LD_LIBRARY_PATH="${install_prefix}/lib64"
+pushd ../../utils/
+source ./imovo-utils-setup-env.sh 'gdev-c-utils'
+popd
