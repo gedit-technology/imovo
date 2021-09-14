dc808df8f1d7444022ab7285a0aa6fc8891782ad
Sébastien Wilmet <sebastien.wilmet@gmail.com>
2021-09-14 20:47:52 +0200

    modules: add gedit.sh

---
+++ src/modules/gedit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if test $# -ne 1
+then
+	echo "$0: wrong number of arguments." 1>&2
+	exit 1
+fi
+
+product="$1"
+
+pushd ../utils/
+./imovo-utils-build-module-standard-meson.sh "${product}" 'gedit' 'master' || exit 1
+popd
