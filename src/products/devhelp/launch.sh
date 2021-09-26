c129c14199e174aca97e0fdc5940b14145cbb8e3
Sébastien Wilmet <swilmet@informatique-libre.be>
2021-09-26 15:49:15 +0200

    products/devhelp: improve launch.sh script

--- src/products/devhelp/launch.sh
+++ src/products/devhelp/launch.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-pushd ../../utils/
+get_script_directory() {
+	script_dir=`dirname "$BASH_SOURCE"`
+	absolute_script_dir=`cd "$script_dir" && pwd`
+	echo "$absolute_script_dir"
+}
+
+script_directory="`get_script_directory`"
+
+pushd "${script_directory}/../../utils/"
 source ./imovo-utils-setup-env.sh 'devhelp'
 popd
 
