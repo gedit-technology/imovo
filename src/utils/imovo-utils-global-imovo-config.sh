c142306a66791ef2087968d67c98ca0d6195408a
Sébastien Wilmet <swilmet@informatique-libre.be>
2021-11-06 20:10:10 +0100

    utils: global-imovo-config: update git_repos_location

--- src/utils/imovo-utils-global-imovo-config.sh
+++ src/utils/imovo-utils-global-imovo-config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-imovo_config_git_repos_location=~/comp/gedit-org
+imovo_config_git_repos_location=~/comp/repos
 
 imovo_config_prefix=~/.imovo
 imovo_config_tarballs_location="${imovo_config_prefix}/_tarballs"
