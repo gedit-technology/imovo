7224bdc9fa5a5f11b1f898807058609281da73e2
Sébastien Wilmet <swilmet@informatique-libre.be>
2022-01-31 13:03:29 +0100

    utils: global-imovo-config: add new variable for git repos cache

--- src/utils/imovo-utils-global-imovo-config.sh
+++ src/utils/imovo-utils-global-imovo-config.sh
@@ -5,3 +5,8 @@ imovo_config_git_repos_orig_location=~/comp/repos
 imovo_config_prefix=~/.imovo
 imovo_config_tarballs_location="${imovo_config_prefix}/_tarballs"
 imovo_config_tmp_dir="${imovo_config_prefix}/_tmp"
+
+# An automatic build (from imovo), for a git repo, can be done in the following
+# location. So not in imovo_config_tmp_dir, because the latter is emptied to
+# build certain modules.
+imovo_config_git_repos_cache_location="${imovo_config_prefix}/_git_repos_cache"
