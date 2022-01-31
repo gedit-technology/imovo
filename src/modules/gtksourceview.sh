08bc7ed176ac0b949811d4c771f3229bbef1ae99
Sébastien Wilmet <swilmet@informatique-libre.be>
2022-01-31 11:27:40 +0100

    utils: rename cache-tarball -> download-tarball-if-needed

--- src/modules/gtksourceview.sh
+++ src/modules/gtksourceview.sh
@@ -8,7 +8,7 @@ tarball_url='https://github.com/gedit-org/gtksourceview/releases/download/4.0.40
 tarball_filename='gtksourceview-4.0.40.tar.xz'
 
 pushd ../utils/
-./imovo-utils-cache-tarball.sh "${tarball_filename}" "${tarball_url}"
+./imovo-utils-download-tarball-if-needed.sh "${tarball_filename}" "${tarball_url}"
 ./imovo-utils-prepare-module-source-tarball.sh "${tarball_filename}"
 popd
 
