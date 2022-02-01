ba1a914f4b161b86064d6dcd022e9354a6327d63
Sébastien Wilmet <swilmet@informatique-libre.be>
2022-02-01 15:33:06 +0100

    modules: add missing `|| exit 1`'s

--- src/modules/gnome-latex.sh
+++ src/modules/gnome-latex.sh
@@ -12,10 +12,10 @@ source ../utils/imovo-utils-global-imovo-config.sh
 install_prefix="${imovo_config_prefix}/${product}"
 
 pushd ../utils/
-./imovo-utils-prepare-module-source-git-repo-cache.sh 'gnome-latex' 'master'
+./imovo-utils-prepare-module-source-git-repo-cache.sh 'gnome-latex' 'master' || exit 1
 popd
 
-pushd "${imovo_config_git_repos_cache_location}/gnome-latex/"
+pushd "${imovo_config_git_repos_cache_location}/gnome-latex/" || exit 1
 ./autogen.sh --prefix="${install_prefix}" --enable-introspection --enable-vala --disable-dconf-migration || exit 1
 make || exit 1
 make check || exit 1 # before install
--- src/modules/gtksourceview.sh
+++ src/modules/gtksourceview.sh
@@ -21,7 +21,7 @@ pushd ../utils/
 ./imovo-utils-prepare-module-source-tarball.sh "${tarball_filename}" || exit 1
 popd
 
-pushd "${imovo_config_tmp_dir}/gtksourceview-4.0.40/"
+pushd "${imovo_config_tmp_dir}/gtksourceview-4.0.40/" || exit 1
 ./configure --prefix="${install_prefix}" --enable-introspection=yes --enable-vala=yes || exit 1
 make || exit 1
 make check || exit 1 # before install
