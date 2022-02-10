5deed818179d84f8a6f3807a6125673ba487e803
Sébastien Wilmet <swilmet@informatique-libre.be>
2022-02-10 17:53:50 +0100

    make-releases: gtksourceview: add "|| exit 1"'s, print success messages

--- src/make-releases/make-release-gtksourceview.sh
+++ src/make-releases/make-release-gtksourceview.sh
@@ -10,15 +10,19 @@ version="$1"
 
 source ../utils/imovo-utils-global-imovo-config.sh
 
-pushd "${imovo_config_git_repos_orig_location}/gtksourceview"
-git clean -Xdf
+pushd "${imovo_config_git_repos_orig_location}/gtksourceview" || exit 1
+git clean -Xdf || exit 1
 
 # My old script had also: --disable-Werror
 ./autogen.sh --enable-introspection=yes --enable-vala=yes --enable-gtk-doc \
-	--enable-glade-catalog
+	--enable-glade-catalog || exit 1
 
-make -j`nproc`
-make -j`nproc` distcheck
-mv "gtksourceview-${version}.tar.xz" "${imovo_config_new_tarballs_location}"
-git reset --hard HEAD
+make -j`nproc` || exit 1
+make -j`nproc` distcheck || exit 1
+mv "gtksourceview-${version}.tar.xz" "${imovo_config_new_tarballs_location}" || exit 1
+git reset --hard HEAD || exit 1
 popd
+
+echo
+echo "OK: tarball rolled successfully."
+echo "Tarball pathname: ${imovo_config_new_tarballs_location}/gtksourceview-${version}.tar.xz"
