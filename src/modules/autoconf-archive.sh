cdf410b5280f1503359e2fb51a47bd9139ea6a0e
Sébastien Wilmet <swilmet@informatique-libre.be>
2022-02-14 17:38:37 +0100

    modules: autoconf-archive: update to version 2022.02.11

--- src/modules/autoconf-archive.sh
+++ src/modules/autoconf-archive.sh
@@ -14,13 +14,13 @@ install_prefix="${imovo_config_prefix}/${product}"
 # Install from a tarball, it's easier because there is the `./configure` script,
 # while in the git repo it's missing (like all standard Autotools projects).
 
-tarball_filename='autoconf-archive-2021.02.19.tar.xz'
+tarball_filename='autoconf-archive-2022.02.11.tar.xz'
 
 pushd ../utils/
 ./imovo-utils-prepare-module-source-tarball.sh "${tarball_filename}" || exit 1
 popd
 
-pushd "${imovo_config_tmp_dir}/autoconf-archive-2021.02.19/" || exit 1
+pushd "${imovo_config_tmp_dir}/autoconf-archive-2022.02.11/" || exit 1
 ./configure --prefix="${install_prefix}" || exit 1
 make || exit 1
 make install || exit 1
