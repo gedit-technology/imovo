f4f7cc55932e231b3fb4469c1d53b157a39e1140
Sébastien Wilmet <swilmet@informatique-libre.be>
2022-04-15 01:48:54 +0200

    modules: re-add an older version of autoconf-archive, 2021.02.19
    Because currently gnome-build-meta uses this version, not the latest one
    from 2022, unfortunately.

---
+++ src/modules/autoconf-archive-2021.02.19.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if test $# -ne 1
+then
+	echo "$0: wrong number of arguments." 1>&2
+	exit 1
+fi
+
+product="$1"
+
+source ../utils/imovo-utils-global-imovo-config.sh
+install_prefix="${imovo_config_prefix}/${product}"
+
+# Install from a tarball, it's easier because there is the `./configure` script,
+# while in the git repo it's missing (like all standard Autotools projects).
+
+tarball_filename='autoconf-archive-2021.02.19.tar.xz'
+
+pushd ../utils/
+./imovo-utils-prepare-module-source-tarball.sh "${tarball_filename}" || exit 1
+popd
+
+pushd "${imovo_config_tmp_dir}/autoconf-archive-2021.02.19/" || exit 1
+./configure --prefix="${install_prefix}" || exit 1
+make || exit 1
+make install || exit 1
+make check || exit 1
+popd
