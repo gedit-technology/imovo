805c3a2605164afa8eb9b8c0fdce7436c16c122d
Sébastien Wilmet <swilmet@informatique-libre.be>
2022-05-24 08:36:18 +0200

    Update autogen_gnome_latex

--- src/utils/imovo-utils-setup-env.sh
+++ src/utils/imovo-utils-setup-env.sh
@@ -24,7 +24,7 @@ export ACLOCAL_PATH="${_install_prefix}/share/aclocal/"
 export PS1="[${_product}] ${PS1}"
 
 alias meson_imovo='meson --prefix "${_install_prefix}"'
-alias autogen_gnome_latex='./autogen.sh --prefix="${_install_prefix}" --enable-introspection=yes --enable-vala=yes --enable-code-coverage --enable-gtk-doc'
+alias autogen_gnome_latex='./autogen.sh --prefix="${_install_prefix}" --enable-introspection=yes --enable-code-coverage --enable-gtk-doc'
 alias autogen_gspell='./autogen.sh --prefix="${_install_prefix}" --enable-introspection=yes --enable-vala=yes --enable-code-coverage --enable-gtk-doc --enable-installed-tests'
 alias autogen_gtksourceview='./autogen.sh --prefix="${_install_prefix}" --enable-introspection=yes --enable-code-coverage --enable-gtk-doc --enable-installed-tests'
 alias make_no_deprecations="make -j`nproc` CFLAGS='-Wno-deprecated-declarations'"
