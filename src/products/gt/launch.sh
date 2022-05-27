66cb38d9a345fdef08ed422eadf2f2011b4fd743
Sébastien Wilmet <swilmet@informatique-libre.be>
2022-05-27 19:13:09 +0200

    products: add launch.sh script for gnome-latex (gt)

---
+++ src/products/gt/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+get_script_directory() {
+	script_dir=`dirname "$BASH_SOURCE"`
+	absolute_script_dir=`cd "$script_dir" && pwd`
+	echo "$absolute_script_dir"
+}
+
+script_directory="`get_script_directory`"
+
+pushd "${script_directory}/../../utils/"
+source ./imovo-utils-setup-env.sh 'gt'
+popd
+
+gnome-latex "$@"
