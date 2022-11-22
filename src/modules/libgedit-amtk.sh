0f0016d7b1a1ee73cc90924f84b2064dcc5d4fc6
Sébastien Wilmet <swilmet@informatique-libre.be>
2022-11-22 16:00:00 +0100

    amtk -> libgedit-amtk

--- src/modules/libgedit-amtk.sh
+++ src/modules/libgedit-amtk.sh
@@ -9,5 +9,5 @@ fi
 product="$1"
 
 pushd ../utils/
-./imovo-utils-build-module-standard-meson.sh "${product}" 'amtk' 'master' || exit 1
+./imovo-utils-build-module-standard-meson.sh "${product}" 'libgedit-amtk' 'main' || exit 1
 popd
