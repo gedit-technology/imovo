a68d8fd5da84ef833300bd77ec55ddb111e60d1a
Sébastien Wilmet <swilmet@informatique-libre.be>
2023-02-21 16:00:00 +0100

    utils: cleanup

--- src/utils/imovo-utils-global-imovo-config.sh
+++ src/utils/imovo-utils-global-imovo-config.sh
@@ -6,9 +6,6 @@ imovo_config_prefix=~/.imovo
 imovo_config_tarballs_location="${imovo_config_prefix}/_tarballs"
 imovo_config_tmp_dir="${imovo_config_prefix}/_tmp"
 
-# No longer used:
-#imovo_config_new_tarballs_location="${imovo_config_prefix}/_new_tarballs"
-
 # An automatic build (from imovo) for a git repo can be done in the following
 # location. So not in imovo_config_tmp_dir, because the latter is emptied each
 # time a module is built.
